4a5342fa851b750a4cddf597aaa9f15d32ce79bb
phd.abel@gmail.com <azimovd04m0483#7>
2018-10-30 16:28:25 -0300

    leitura dos documentos e indexacao

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ var indexObj = function(res){
         date.setDate(d);
         date.setMonth(m-1);
         let index = "document";
-        let type = (res["ARTICLETYPE"] === undefined) ? "unlabeled" : res["ARTICLETYPE"];
+        let type = 'news';
         let id = res["DOCID"];
         let obj = {
             date : date,
@@ -38,7 +38,7 @@ var indexObj = function(res){
             byline : (res["BYLINE"] === undefined) ? null : res["BYLINE"],
             edition : parseInt(res["EDITION"]),
             page : parseInt(res["PAGE"]),
-            articletype : (res["ARTICLETYPE"] === undefined) ? "unlabeled" : res["ARTICLETYPE"],
+            articletype : (res["ARTICLETYPE"] === undefined) ? null : res["ARTICLETYPE"],
             graphic : (res["GRAPHIC"] === undefined) ? null : res["GRAPHIC"],
             recordno : res["RECORDNO"],
             text : res["TEXT"]
@@ -52,27 +52,46 @@ var indexObj = function(res){
     });
 };
 
-fs.readdirSync(target).forEach(function(file){
-    if(file.indexOf('.sgml') != -1){
-        let doc = target+file;
-        let data = "<ROOT>\\n"+fs.readFileSync(doc, "utf8")+"\\n</ROOT>";
+function readFilePromise(doc){
+    return new Promise(function(fulfill,reject){
+        fs.readFile(doc, "utf8",function(err,data){
+            if(err) reject(err);
+            else fulfill("<ROOT>\\n"+data+"\\n</ROOT>");
+        });
+    });
+};
+
+function reading(doc){
+    readFilePromise(doc)
+    .then(function(data){
         parser.parseString(data)
-            .then(function(res){
-                if(res === null) console.log("null exception");
-                else {
-                    for(var i = 0; i < res["DOC"].length; i++){
-                        indexObj(res["DOC"][i]).then(function(idx){ 
-                            conn.create(idx.index, idx.type, idx.id, idx.body)
-                                    .then(function(res){
-                                        console.log(res);
-                                    },function(err){
-                                        console.log(err);
-                                    });
-                        });
-                    }
+        .then(function(res){
+            if(res === null) console.log("null exception");
+            else {
+                for(var i = 0; i < res["DOC"].length; i++){
+                    indexObj(res["DOC"][i]).then(function(idx){ 
+                        conn.create(idx.index, idx.type, idx.id, idx.body)
+                                .then(function(res){
+                                    console.log("\\nok\\n");
+                                },function(err){
+                                    console.log("err");
+                                });
+                    });
                 }
-            },function(err){
-                console.log(err);
-            });
+            }
+        });
+    });
+}
+
+fs.readdir(target, function(err,files){
+    if(err) console.log(err);
+    else {
+        for(var i = 0; i < files.length; i++){
+            if(files[i].indexOf('.sgml') !== -1){
+                let doc = target + files[i];
+                setTimeout(reading, i*1000, doc);
+            }
+        }
     }
 });
+
